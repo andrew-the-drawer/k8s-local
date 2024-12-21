af458f856b994537333bf504d1ee496a7d08b00a
Trung Nguyen <lantrungseo@gmail.com>
2024-12-21 23:52:47 +0700

    fix: remove interactive cp in master script

--- kubeadm/master.sh
+++ kubeadm/master.sh
@@ -7,7 +7,7 @@ read -p "Enter the public IP address of the master node: " MASTER_IP
 # the `--pod-network-cidr` is always 10.224.0.0/16 as it is required for Flannel CNI (0.26.2) default config
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$MASTER_IP
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Install Flannel CNI, version 0.26.2 (latest at that time)
